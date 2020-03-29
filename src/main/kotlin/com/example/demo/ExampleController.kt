fabfcba1f79782d9ffcd3093f8d7b4048f53821a
Pedro Felix <pmhsfelix@gmail.com>
2020-03-29 15:00:30 +0100

    changes hello message

--- src/main/kotlin/com/example/demo/ExampleController.kt
+++ src/main/kotlin/com/example/demo/ExampleController.kt
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController
 class ExampleController {
 
     @GetMapping(produces = ["text/plain"])
-    fun get() = "Hello Web"
+    fun get() = "Hello Web, from Cloud Run"
 }
