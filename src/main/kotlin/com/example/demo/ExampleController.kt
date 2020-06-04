7b25c3693c2dfc7e2be1afd1a666d7adebae1bee
João Pereira <47888471+joao-pereira5@users.noreply.github.com>
2020-06-04 10:11:04 +0100

    Test deploy on ion-dev

--- src/main/kotlin/com/example/demo/ExampleController.kt
+++ src/main/kotlin/com/example/demo/ExampleController.kt
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController
 class ExampleController {
 
     @GetMapping(produces = ["text/plain"])
-    fun get() = "Goodbye world"
+    fun get() = "Testing "
 }
